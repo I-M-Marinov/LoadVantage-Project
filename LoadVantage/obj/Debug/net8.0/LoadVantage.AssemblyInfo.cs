6393bd94646c3665ab87fa7d842781fc3762ad24
I-M-Marinov <lcfrrr@gmail.com>
2024-11-02 22:57:29 +0200

    Changes on the Broker Views ( buttons, input fields arrangement, general UI colors )

--- LoadVantage/obj/Debug/net8.0/LoadVantage.AssemblyInfo.cs
+++ LoadVantage/obj/Debug/net8.0/LoadVantage.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25b61069b93da7228b94e26dffa9bc1c1ab565f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2404106a27a0cc0b199fab85d862ed4294fee31e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
