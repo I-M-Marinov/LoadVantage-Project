2ca1c01b0951a163d6f35e406ccbabfe6e00b28a
I-M-Marinov <lcfrrr@gmail.com>
2024-11-05 14:22:44 +0200

    Updated the LoadViewModel with the DisplayName attribute

--- LoadVantage.Core/Models/Load/LoadViewModel.cs
+++ LoadVantage.Core/Models/Load/LoadViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using LoadVantage.Core.ValidationAttributes;
 using LoadVantage.Infrastructure.Data.Services;
 using static LoadVantage.Common.ValidationConstants.LoadValidations;
@@ -7,46 +8,56 @@ namespace LoadVantage.Core.Models.Load
 {
     public class LoadViewModel
     {
-        public Guid? Id { get; set; } 
+        public Guid Id { get; set; } 
 
         [Required(ErrorMessage = OriginCityRequired)]
+        [DisplayName("Origin city")]
         [StringLength(LoadOriginCityMaxLength, MinimumLength = LoadOriginCityMinLength, ErrorMessage = OriginCityLengthNotValid)]
 
         public string OriginCity { get; set; } = null!;
 
         [Required(ErrorMessage = OriginStateRequired)]
+        [DisplayName("Origin state")]
         [StringLength(StateMinMaxLength, ErrorMessage = StateLengthNotValid)]
 
         public string OriginState { get; set; } = null!;
 
         [Required(ErrorMessage = DestinationCityRequired)]
+        [DisplayName("Destination city")]
         [StringLength(LoadDestinationCityMaxLength, MinimumLength = LoadDestinationCityMinLength, ErrorMessage = DestinationCityLengthNotValid)]
 
         public string DestinationCity { get; set; } = null!;
 
         [Required(ErrorMessage = DestinationStateRequired)]
+        [DisplayName("Destination state")]
         [StringLength(StateMinMaxLength, ErrorMessage = StateLengthNotValid)]
         public string DestinationState { get; set; } = null!;
 
         [Required(ErrorMessage = PickupTimeRequired)]
+        [DisplayName("Pickup time")]
         [DataType(DataType.DateTime, ErrorMessage = PickupTimeInvalidFormat)]
         public DateTime PickupTime { get; set; }
 
         [Required(ErrorMessage = DeliveryTimeRequired)]
+        [DisplayName("Delivery time")]
         [DataType(DataType.DateTime, ErrorMessage = DeliveryTimeInvalidFormat)]
         [DateGreaterThan(nameof(PickupTime), ErrorMessage = DeliveryTimeCannotBeBeforePickupTime)] // Custom attribute  
         public DateTime DeliveryTime { get; set; }
 
+        [DisplayName("Distance")]
         public double? Distance { get; set; }
 
         [Required(ErrorMessage = PriceRequired)]
+        [DisplayName("Price")]
         [Range(LoadPriceMinValue, LoadPriceMaxValue, ErrorMessage = PriceRangeInvalid)]
         public decimal PostedPrice { get; set; }
 
         [Required(ErrorMessage = WeightRequired)]
+        [DisplayName("Weight")]
         [Range(WeightMinValue, WeightMaxValue, ErrorMessage = WeightRangeInvalid)]
         public double Weight { get; set; }
 
+        [DisplayName("Status")]
         public string? Status { get; set; } 
         public Guid BrokerId { get; set; }
         public Guid? DispatcherId { get; set; }
