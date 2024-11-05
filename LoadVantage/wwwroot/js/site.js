d6bdef32ccdbb8f3a9e070e7650e465551b85415
I-M-Marinov <lcfrrr@gmail.com>
2024-11-06 00:11:18 +0200

    Added a function that handles fading out of error and success messages + CSS for the fade-out effect

--- LoadVantage/wwwroot/js/site.js
+++ LoadVantage/wwwroot/js/site.js
@@ -3,3 +3,26 @@
 
 // Write your JavaScript code.
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Error message fade-out
+    var errorMessage = document.getElementById('error-message');
+    if (errorMessage) {
+        setTimeout(function () {
+            errorMessage.classList.add('fade-out');
+        }, 1000); 
+        setTimeout(function () {
+            errorMessage.style.display = 'none'; 
+        }, 3500); 
+    }
+
+    // Success message fade-out
+    var successMessage = document.getElementById('success-message');
+    if (successMessage) {
+        setTimeout(function () {
+            successMessage.classList.add('fade-out');
+        }, 1000); 
+        setTimeout(function () {
+            successMessage.style.display = 'none'; 
+        }, 3500); 
+    }
+});
