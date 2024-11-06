5227ae0b96d6e56d01381350cc9cc7170af3d82c
I-M-Marinov <lcfrrr@gmail.com>
2024-11-06 09:33:06 +0200

    New favicon for the app

--- LoadVantage.Core/obj/Debug/net8.0/LoadVantage.Core.AssemblyInfo.cs
+++ LoadVantage.Core/obj/Debug/net8.0/LoadVantage.Core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadVantage.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a6e2c7fba0845a8a504bb59727b894fccf3c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b628c813f81c8decbd0cad912422ff2abbc35a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadVantage.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadVantage.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LoadVantage/obj/Debug/net8.0/LoadVantage.AssemblyInfo.cs
+++ LoadVantage/obj/Debug/net8.0/LoadVantage.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a6e2c7fba0845a8a504bb59727b894fccf3c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b628c813f81c8decbd0cad912422ff2abbc35a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
