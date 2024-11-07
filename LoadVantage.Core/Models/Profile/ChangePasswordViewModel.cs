45f6d9500549566cad5a898fd7e7739ad75bebb7
I-M-Marinov <lcfrrr@gmail.com>
2024-11-08 01:49:54 +0200

    Added the new ChangePasswordViewModel to work with the ChangePasswordAsync method in the ProfileService and ChangePassword in the Controller

---
+++ LoadVantage.Core/Models/Profile/ChangePasswordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static LoadVantage.Common.ValidationConstants.UserValidations;
+
+
+namespace LoadVantage.Core.Models.Profile
+{
+	public class ChangePasswordViewModel
+	{
+		[Required(ErrorMessage = "Current password is required.")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Current Password")]
+		public string CurrentPassword { get; set; } = null!;
+
+		[Required(ErrorMessage = "New password is required.")]
+		[StringLength(NewPasswordMaxLength, MinimumLength = NewPasswordMinLength, ErrorMessage = NewPasswordLengthNotValid)]
+		[DataType(DataType.Password)]
+		[Display(Name = "New Password")]
+		public string NewPassword { get; set; } = null!;
+
+		[Required(ErrorMessage = "Confirmation of the new password is required.")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm New Password")]
+		[Compare("NewPassword", ErrorMessage = NewAndConfirmPasswordDoNotMatch)]
+		public string ConfirmPassword { get; set; } = null!;
+	}
+}
