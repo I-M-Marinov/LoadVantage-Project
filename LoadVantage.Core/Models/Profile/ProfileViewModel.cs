5d1a5685c6c8e101f6a80c6e7ba01f44bd6b6627
I-M-Marinov <lcfrrr@gmail.com>
2024-11-07 10:05:05 +0200

    Added validation Error Messages in the ProfileViewModel

--- LoadVantage.Core/Models/Profile/ProfileViewModel.cs
+++ LoadVantage.Core/Models/Profile/ProfileViewModel.cs
@@ -8,28 +8,28 @@ namespace LoadVantage.Core.Models.Profile
     public class ProfileViewModel
     {
         [Required]
-        public string Id { get; set; } = null!;
-        [Required]
+        public required string Id { get; set; } = null!;
+        [Required(ErrorMessage = "Username is required.")]
         [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength, ErrorMessage = UserNameLengthNotValid)]
-        public string Username { get; set; } = null!;
-        [Required]
+        public required string Username { get; set; } = null!;
+        [Required(ErrorMessage = "Email name is required.")]
         [StringLength(EmailMaxLength, MinimumLength = EmailMinLength, ErrorMessage = EmailLengthNotValid)]
-        public string Email { get; set; } = null!;
-        [Required]
+        public required string Email { get; set; } = null!;
+        [Required(ErrorMessage = "First name is required.")]
         [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength, ErrorMessage = FirstNameLengthNotValid)]
-        public string FirstName { get; set; } = null!;
-        [Required]
+        public required string FirstName { get; set; } = null!;
+        [Required(ErrorMessage = "Last name is required.")]
         [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength, ErrorMessage = LastNameLengthNotValid)]
-        public string LastName { get; set; } = null!;
-        [Required]
+        public required string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "Company name is required.")]
         [StringLength(CompanyNameMaxLength, MinimumLength = CompanyNameMinLength, ErrorMessage = CompanyNameLengthNotValid)]
-        public string CompanyName { get; set; } = null!;
+        public required string CompanyName { get; set; } = null!;
         [Required]
         [StringLength(PositionMaxLength, MinimumLength = PositionMinLength, ErrorMessage = PositionLengthNotValid)]
-        public string Position { get; set; } = null!;
-        [Required]
+        public required string Position { get; set; } = null!;
+        [Required(ErrorMessage = "Phone number is required.")]
         [StringLength(UserPhoneNumberMaxLength, MinimumLength = UserPhoneNumberMinLength, ErrorMessage = UserPhoneNumberLengthNotValid)]
-        public string PhoneNumber { get; set; } = null!;
+        public required string PhoneNumber { get; set; } = null!;
         public string FullName => $"{FirstName} {LastName}";
     }
 }
