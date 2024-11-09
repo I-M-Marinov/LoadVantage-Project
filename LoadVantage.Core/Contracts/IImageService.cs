9c623112ba92b15b23146df03a359a3eb32bb444
I-M-Marinov <lcfrrr@gmail.com>
2024-11-09 23:14:07 +0200

    Added the Image Service ( Cloudinary Media Library service ) to handle user images

---
+++ LoadVantage.Core/Contracts/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using LoadVantage.Core.Models.Image;
+
+namespace LoadVantage.Core.Contracts
+{
+	public interface IImageService
+	{
+		Task<ImageUploadResult> UploadImageAsync(IFormFile file);
+        Task<DeleteImageResult> DeleteImageAsync(string publicId);
+
+    }
+}
