13ad0f3e815e16ac0ff4c780d310cd500ab25869
I-M-Marinov <lcfrrr@gmail.com>
2024-11-12 00:56:18 +0200

    Removed the Broker and Dispatcher areas and logic was moved to the newly created LoadController, ProfileController, LoadBoardController,

--- LoadVantage/Areas/Broker/Contracts/IBrokerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using LoadVantage.Core.Models.Profile;
-
-namespace LoadVantage.Areas.Broker.Contracts
-{
-    public interface IBrokerService
-    {
-        Task<ProfileViewModel> GetBrokerInformationAsync(string userId);
-    }
-}
