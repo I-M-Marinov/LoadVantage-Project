eca2b613e1454a38f6229938d120c46d8b2d4b64
I-M-Marinov <lcfrrr@gmail.com>
2024-11-11 10:55:55 +0200

    Updated the ProfileViewModel adding the ChangePasswordViewModel and ImageFileUploadModel so they can be passed to the appropriate Views when rendering the Profile View ( and all it's Partial views )
    -- Added custom validation so depending on if user is changing his password, editing his profile or uploading / deleting a new profile image

--- LoadVantage.Core/Models/Profile/ProfileViewModel.cs
+++ LoadVantage.Core/Models/Profile/ProfileViewModel.cs
@@ -1,12 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using LoadVantage.Core.ValidationAttributes;
-using Microsoft.AspNetCore.Identity;
+
+using LoadVantage.Core.Models.Image;
 using static LoadVantage.Common.GeneralConstants.UserImage;
 using static LoadVantage.Common.ValidationConstants.UserValidations;
 
 namespace LoadVantage.Core.Models.Profile
 {
-    public class ProfileViewModel
+    public class ProfileViewModel :IValidatableObject
     {
         public  string Id { get; set; } = null!;
         [Required(ErrorMessage = "Username is required.")]
@@ -36,7 +36,51 @@ namespace LoadVantage.Core.Models.Profile
 		[StringLength(UserImageMaxLength, ErrorMessage = ImageUrlExceedsLimit)]
 		public string? UserImageUrl { get; set; }
 
-		public ChangePasswordViewModel? ChangePasswordViewModel { get; set; } = null!;
+		public ChangePasswordViewModel? ChangePasswordViewModel { get; set; } 
+		public ImageFileUploadModel? ImageFileUploadModel { get; set; } 
 
-    }
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			var results = new List<ValidationResult>();
+
+			bool isEditingProfile = ChangePasswordViewModel == null && ImageFileUploadModel == null;
+			bool isChangingPassword = ChangePasswordViewModel != null;
+			bool isUploadingImage = ImageFileUploadModel?.FormFile != null;
+
+			if (isEditingProfile)
+			{
+				Validator.TryValidateProperty(Id, new ValidationContext(this) { MemberName = nameof(Id) }, results);
+				Validator.TryValidateProperty(Username, new ValidationContext(this) { MemberName = nameof(Username) }, results);
+				Validator.TryValidateProperty(Email, new ValidationContext(this) { MemberName = nameof(Email) }, results);
+				Validator.TryValidateProperty(FirstName, new ValidationContext(this) { MemberName = nameof(FirstName) }, results);
+				Validator.TryValidateProperty(LastName, new ValidationContext(this) { MemberName = nameof(LastName) }, results);
+				Validator.TryValidateProperty(CompanyName, new ValidationContext(this) { MemberName = nameof(CompanyName) }, results);
+				Validator.TryValidateProperty(Position, new ValidationContext(this) { MemberName = nameof(Position) }, results);
+				Validator.TryValidateProperty(PhoneNumber, new ValidationContext(this) { MemberName = nameof(PhoneNumber) }, results);
+			}
+			else if (isChangingPassword)
+			{
+				// Validate ChangePasswordViewModel only
+				if (ChangePasswordViewModel != null)
+				{
+					var passwordResults = new List<ValidationResult>();
+					Validator.TryValidateObject(ChangePasswordViewModel, new ValidationContext(ChangePasswordViewModel), passwordResults, true);
+					results.AddRange(passwordResults);
+				}
+			}
+			else if (isUploadingImage)
+			{
+				// Validate ImageFileUploadModel only
+				if (ImageFileUploadModel != null)
+				{
+					var imageResults = new List<ValidationResult>();
+					Validator.TryValidateObject(ImageFileUploadModel, new ValidationContext(ImageFileUploadModel), imageResults, true);
+					results.AddRange(imageResults);
+				}
+			}
+
+			return results;
+		}
+
+	}
 }
