8fb008632d95718bd5fc883fa56e131a3328a3ea
I-M-Marinov <lcfrrr@gmail.com>
2024-11-13 01:14:11 +0200

    Created the LoadHub that would serve as a Hub for SignalR's web socket connections to send updates ( when a load is posted, when a load is unposted )

---
+++ LoadVantage/Hubs/LoadHub.cs
@@ -0,0 +1,17 @@
+﻿namespace LoadVantage.Hubs
+{
+	using Microsoft.AspNetCore.SignalR;
+
+	public class LoadHub : Hub
+	{
+		public async Task SendLoadPostedNotification()
+		{
+			await Clients.All.SendAsync("ReceiveLoadPostedNotification");
+		}
+
+		public async Task SendLoadStatusChangedNotification()
+		{
+			await Clients.All.SendAsync("ReceiveLoadUnpostedNotification");
+		}
+	}
+}
