0bd247ff10f4c087750f9e33ca91700a85fd80ae
I-M-Marinov <lcfrrr@gmail.com>
2024-11-12 10:18:55 +0200

    Moved the logic from the broker and dispatcher load board controllers to the newly created LoadBoardController

---
+++ LoadVantage/Controllers/LoadBoardController.cs
@@ -0,0 +1,29 @@
+﻿using LoadVantage.Core.Contracts;
+using LoadVantage.Core.Services;
+using LoadVantage.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadVantage.Controllers
+{
+	public class LoadBoardController(ILoadBoardService loadBoardService) : Controller
+	{
+
+		[HttpGet]
+		public async Task<IActionResult> LoadBoard()
+		{
+			Guid? userId = User.GetUserId();
+			var loadBoardInfo = await loadBoardService.GetLoadBoardAsync(userId.Value);
+
+			return View(loadBoardInfo);
+
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult ReturnToLoadBoard()
+		{
+			return RedirectToAction(nameof(LoadBoard));
+		}
+	}
+}
+
