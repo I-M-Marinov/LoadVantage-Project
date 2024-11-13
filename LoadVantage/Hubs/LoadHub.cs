75e5c2e2374b6be65c5303b607aeb7b8ed928f71
I-M-Marinov <lcfrrr@gmail.com>
2024-11-13 21:05:50 +0200

    Update LoadHub to send update to Clients to Reload the posted loads table on the Load Board

--- LoadVantage/Hubs/LoadHub.cs
+++ LoadVantage/Hubs/LoadHub.cs
@@ -1,17 +1,20 @@
-﻿namespace LoadVantage.Hubs
+﻿using LoadVantage.Core.Models.Load;
+
+namespace LoadVantage.Hubs
 {
 	using Microsoft.AspNetCore.SignalR;
 
 	public class LoadHub : Hub
 	{
-		public async Task SendLoadPostedNotification()
+		public async Task SendLoadPostedNotification(LoadViewModel load)
 		{
-			await Clients.All.SendAsync("ReceiveLoadPostedNotification");
+			await Clients.All.SendAsync("ReceiveLoadPostedNotification", load.Id);
 		}
 
+		// When any changes Statuses for Loads in the Posted Loads table send a notification to clients to reload it, so they are able to see the actual available ones 
 		public async Task SendLoadStatusChangedNotification()
 		{
-			await Clients.All.SendAsync("ReceiveLoadUnpostedNotification");
+			await Clients.All.SendAsync("ReloadPostedLoadsTable");
 		}
 	}
 }
