1a0a1b7d55c0c8d16178d14b70910041f6091b6a
I-M-Marinov <lcfrrr@gmail.com>
2024-11-14 17:49:48 +0200

    Removed a duplicate Logout form in the HeaderPartialView

--- LoadVantage/obj/Debug/net8.0/LoadVantage.AssemblyInfo.cs
+++ LoadVantage/obj/Debug/net8.0/LoadVantage.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a814e7dcb223d327676d96ea793b4ff3fd84238a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+392898525ce40c2e48b6b7964fa7a50eade0c1b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadVantage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
