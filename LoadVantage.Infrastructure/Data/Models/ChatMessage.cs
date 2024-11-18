99074d39230f1def14ca4162226dddac15275ff4
I-M-Marinov <lcfrrr@gmail.com>
2024-11-18 22:53:12 +0200

    Added the ChatMessage Entity to the DB

---
+++ LoadVantage.Infrastructure/Data/Models/ChatMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadVantage.Infrastructure.Data.Models
+{
+	public class ChatMessage
+	{
+		[Required]
+		public Guid Id { get; set; } = Guid.NewGuid();
+
+		[Required]
+		public Guid SenderId { get; set; }
+		[Required]
+		public User Sender { get; set; }
+		[Required]
+		public Guid ReceiverId { get; set; }
+		[Required]
+		public User Receiver { get; set; }
+
+		[Required]
+		[MaxLength(2000)]
+		public string Message { get; set; }
+
+		public DateTime Timestamp { get; set; }
+
+		public bool IsRead { get; set; } = false;
+	}
+}
