707afbb0e32da2c4557c5fa3c4c7cdf4e1fa206c
I-M-Marinov <lcfrrr@gmail.com>
2024-11-21 23:27:13 +0200

    Updated the Truck entity
    -- Removed the Type property
    -- Added the IsActive bool property
    -- Updated the Id Primary key to Guid instead of int

--- LoadVantage.Infrastructure/Data/Models/Truck.cs
+++ LoadVantage.Infrastructure/Data/Models/Truck.cs
@@ -6,10 +6,7 @@ namespace LoadVantage.Infrastructure.Data.Models
 	public class Truck
 	{
 		[Key]
-		public int Id { get; set; }
-
-		[Required]
-		public string Type { get; set; } = null!; // Dry-Van, Reefer, Flat-Bed
+		public Guid Id { get; set; }
 
 		[Required] 
 		public string TruckNumber { get; set; } = null!;
@@ -24,11 +21,10 @@ namespace LoadVantage.Infrastructure.Data.Models
 		public Guid DispatcherId { get; set; }
 		[ForeignKey(nameof(DispatcherId))]
 		public virtual Dispatcher Dispatcher { get; set; } = null!;
-
-		[Required]
 		public Guid? DriverId { get; set; }
 		public virtual Driver? Driver { get; set; } = null!;
 		public bool IsAvailable { get; set; } = true;
+		public bool IsActive { get; set; } = true;
 
 
 	}
