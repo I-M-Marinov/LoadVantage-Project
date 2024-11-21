9341a49c9a0f3984a1e44eec51105808876e47e7
I-M-Marinov <lcfrrr@gmail.com>
2024-11-21 23:34:21 +0200

    Crated the TrucksViewModel
    -- Holds the ProfileViewModel to pass to the Header and Sidebar
    -- List of all trucks for the current Dispatcher
    -- TruckViewModel NewTruck to add a truck via the modal in the ShowTrucks View
    -- TruckViewModel EditedTruck to pass the information from the ShowTrucks View ( from the edit truck modal ) to the EditTruck method in the TruckController

---
+++ LoadVantage.Core/Models/Truck/TrucksViewModel.cs
@@ -0,0 +1,20 @@
+﻿using LoadVantage.Core.Models.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadVantage.Core.Models.Truck
+{
+	public class TrucksViewModel
+	{
+		public ProfileViewModel Profile { get; set; }
+
+		public List<TruckViewModel> Trucks { get; set; }
+
+		public TruckViewModel NewTruck { get; set; } // For the Add Truck form
+		public TruckViewModel EditedTruck { get; set; } // For the Edit Truck form
+
+	}
+}
