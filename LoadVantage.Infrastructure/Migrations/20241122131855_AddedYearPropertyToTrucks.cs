e5aff7b6fe07f3a17fe843b9fa66de4b564b0ab7
I-M-Marinov <lcfrrr@gmail.com>
2024-11-22 21:30:46 +0200

    Added a new migration to the DB adding a new property to the Truck entity ----> Year

---
+++ LoadVantage.Infrastructure/Migrations/20241122131855_AddedYearPropertyToTrucks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoadVantage.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedYearPropertyToTrucks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Trucks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Trucks");
+        }
+    }
+}
