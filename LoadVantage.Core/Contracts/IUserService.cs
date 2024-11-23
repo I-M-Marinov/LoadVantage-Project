9bc173f0ac3f62d846b21d3f3bfd9639c7b58da8
I-M-Marinov <lcfrrr@gmail.com>
2024-11-23 21:26:29 +0200

    Major update of the User Service and User Service Interface
    -- Added 8 new methods that would use the dependency of the User Manager to retrieve User by Id, by Email, by UserName, Create a user, Assign Role to users, Add a claim for user ( also a method that could add multiple claims for the user ) , Get all claims for a user
    
    -- Added summaries for all methods in the IUserService interface

--- LoadVantage.Core/Contracts/IUserService.cs
+++ LoadVantage.Core/Contracts/IUserService.cs
@@ -1,23 +1,82 @@
-﻿using LoadVantage.Core.Models.Chat;
+﻿using System.Security.Claims;
+using LoadVantage.Core.Models.Chat;
 using LoadVantage.Core.Models.Profile;
 using LoadVantage.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace LoadVantage.Core.Contracts
 {
     public interface IUserService
     {
-        Task<User> GetUserByIdAsync(Guid userId);
-        Task<ProfileViewModel> GetUserInformation(Guid userId);
-        Task<UserChatViewModel> GetChatUserInfoAsync(Guid brokerId);
+	    /// <summary>
+	    /// Retrieves a user by his id
+	    /// </summary>
+		Task<User?> GetUserByIdAsync(Guid userId);
+	    /// <summary>
+	    /// Retrieves the currently authorized User or returns null if not authorized.
+	    /// </summary>
+		Task<User?> GetCurrentUserAsync();
+	    /// <summary>
+	    /// Finds the User by his email address
+	    /// </summary>
+		Task<User?> FindUserByEmailAsync(string email);
+	    /// <summary>
+	    /// Finds the User by his username 
+	    /// </summary>
+		Task<User?> FindUserByUsernameAsync(string username);
+	    /// <summary>
+	    /// Creates a new User 
+	    /// </summary>
+		Task<IdentityResult> CreateUserAsync(User user, string password);
+	    /// <summary>
+	    /// Assigns the User role 
+	    /// </summary>
+		Task<IdentityResult> AssignUserRoleAsync(User user, string role);
+	    /// <summary>
+	    /// Retrieve all the claims for a user 
+	    /// </summary>
+	    Task<IEnumerable<Claim>> GetUserClaimsAsync(User user);
+	    /// <summary>
+	    /// Adds a User Claim  
+	    /// </summary>
+		Task<IdentityResult> AddUserClaimAsync(User user, Claim claim);
+		/// <summary>
+		/// Adds multiple User Claims 
+		/// </summary>
+		Task<IdentityResult> AddUserClaimsAsync(User user, IEnumerable<Claim> claims);
+		/// <summary>
+		/// Gets the User information and maps it to the model for the profile section
+		/// </summary>
+		Task<ProfileViewModel> GetUserInformation(Guid userId);
+		/// <summary>
+		/// Gets the User information and maps some of it the model for the char section
+		/// </summary>
+		Task<UserChatViewModel> GetChatUserInfoAsync(Guid brokerId);
+		/// <summary>
+		/// Retrieves all registered Users
+		/// </summary>
 		Task<IEnumerable<User>> GetAllUsersAsync();
-        Task<IEnumerable<User>> GetDispatchersAsync();
-        Task<IEnumerable<User>> GetBrokersAsync();
-        Task UpdateUserPositionAsync(Guid userId, string position);
-        Task AssignUserRoleAsync(Guid userId, string role);
-        Task UpdateUserImageAsync(Guid userId, IFormFile file);
-        Task DeleteUserImageAsync(Guid userId, Guid imageUrl);
-
+		/// <summary>
+		/// Retrieves all registered Dispatchers 
+		/// </summary>
+		Task<IEnumerable<User>> GetDispatchersAsync();
+		/// <summary>
+		/// Retrieves all registered Brokers 
+		/// </summary>
+		Task<IEnumerable<User>> GetBrokersAsync();
+		/// <summary>
+		/// Updates the User's position  
+		/// </summary>
+		Task UpdateUserPositionAsync(Guid userId, string position);
+		/// <summary>
+		/// Updates the User's image  
+		/// </summary>
+		Task UpdateUserImageAsync(Guid userId, IFormFile file);
+		/// <summary>
+		/// Removes the User's image and sets the default user image in its place
+		/// </summary>
+		Task DeleteUserImageAsync(Guid userId, Guid imageUrl);
 
     }
 
--- LoadVantage.Core/Services/UserService.cs
+++ LoadVantage.Core/Services/UserService.cs
@@ -4,11 +4,13 @@ using LoadVantage.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using System.Security.Claims;
 using LoadVantage.Core.Models.Image;
 using LoadVantage.Core.Models.Profile;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using static LoadVantage.Common.GeneralConstants.UserImage;
+using static LoadVantage.Common.GeneralConstants.ErrorMessages;
 using LoadVantage.Core.Models.Chat;
 
 
@@ -21,62 +23,110 @@ namespace LoadVantage.Core.Services
     public class UserService : IUserService
     {
         private readonly UserManager<User> userManager;
-        private readonly LoadVantageDbContext context;
+        private readonly IHttpContextAccessor httpContextAccessor;
+		private readonly LoadVantageDbContext context;
         private readonly IImageService imageService;
 
-        public UserService(UserManager<User> _userManager, LoadVantageDbContext _context, IImageService _imageService)
+        public UserService(UserManager<User> _userManager, IHttpContextAccessor _httpContextAccessor, LoadVantageDbContext _context, IImageService _imageService)
 		{
             userManager = _userManager;
-            context = _context;
+            httpContextAccessor = _httpContextAccessor;
+			context = _context;
             imageService = _imageService;
 		}
 
-		public async Task<UserChatViewModel> GetChatUserInfoAsync(Guid userId)
-		{
-			var broker = await context.Users
-				.Include(u => u.UserImage) 
-				.Where(u => u.Id == userId)
-				.Select(u => new UserChatViewModel
-				{
-                    Id = u.Id,
-					FullName = u.FullName,
-					ProfilePictureUrl = u.UserImage.ImageUrl, 
-					PhoneNumber = u.PhoneNumber, 
-					Company = u.CompanyName 
-				})
-				.AsNoTracking()
-				.FirstOrDefaultAsync();
-
-			return broker;
-		}
+		
 
 		public async Task<User> GetUserByIdAsync(Guid userId)
         {
             return await userManager.FindByIdAsync(userId.ToString());
         }
 
-        public async Task<ProfileViewModel> GetUserInformation(Guid userId)
+        public async Task<User> GetCurrentUserAsync()
         {
-	        var user =  await GetUserByIdAsync(userId);
+	        var userId = httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-	        var viewModel = new ProfileViewModel
+	        if (string.IsNullOrEmpty(userId))
 	        {
-		        Id = user.Id.ToString(),
-		        Username = user.UserName!,
-		        Email = user.Email!,
-		        FirstName = user.FirstName,
-		        LastName = user.LastName,
-		        CompanyName = user.CompanyName!,
-		        Position = user.Position!,
-		        PhoneNumber = user.PhoneNumber!,
-		        UserImageUrl = user.UserImage!.ImageUrl,
-		        ChangePasswordViewModel = new ChangePasswordViewModel(),
-		        ImageFileUploadModel = new ImageFileUploadModel()
-	        };
+		        return null;
+			}
 
-	        return viewModel;
-
+			return await userManager.FindByIdAsync(userId);
         }
+
+		public async Task<User> FindUserByEmailAsync(string email)
+		{
+			if (string.IsNullOrWhiteSpace(email))
+				throw new ArgumentException("Email cannot be null or empty.", nameof(email));
+
+			return await userManager.FindByEmailAsync(email);
+		}
+
+		public async Task<User> FindUserByUsernameAsync(string username)
+		{
+			if (string.IsNullOrWhiteSpace(username))
+				throw new ArgumentException("Username cannot be null or empty.", nameof(username));
+
+			return await userManager.FindByNameAsync(username);
+		}
+
+		public async Task<IdentityResult> CreateUserAsync(User user, string password)
+		{
+			if (user == null)
+				throw new ArgumentNullException(nameof(user));
+
+			if (string.IsNullOrWhiteSpace(password))
+				throw new ArgumentException("Password cannot be null or empty.", nameof(password));
+
+			return await userManager.CreateAsync(user, password);
+		}
+
+		public async Task<IdentityResult> AssignUserRoleAsync(User user, string role)
+		{
+
+			if (user == null)
+				throw new ArgumentNullException(nameof(user));
+
+			if (string.IsNullOrWhiteSpace(role))
+				throw new ArgumentException("Role cannot be null or empty.", nameof(role));
+
+			var isAlreadyInRole = await userManager.IsInRoleAsync(user, role);
+
+			if (!isAlreadyInRole)
+			{
+				return await userManager.AddToRoleAsync(user, role);
+			}
+
+			throw new ArgumentException("That role is already assigned to the user.", nameof(role));
+
+		}
+		public async Task<IEnumerable<Claim>> GetUserClaimsAsync(User user)
+		{
+			var claims = await userManager.GetClaimsAsync(user);
+			return claims;
+		}
+
+		public async Task<IdentityResult> AddUserClaimAsync(User user, Claim claim)
+		{
+			if (user == null)
+				throw new ArgumentNullException(nameof(user));
+
+			if (claim == null)
+				throw new ArgumentNullException(nameof(claim));
+
+			return await userManager.AddClaimAsync(user, claim);
+		}
+
+		public async Task<IdentityResult> AddUserClaimsAsync(User user, IEnumerable<Claim> claims)
+		{
+			if (user == null)
+				throw new ArgumentNullException(nameof(user));
+
+			if (claims == null || !claims.Any())
+				throw new ArgumentException("Claims cannot be null or empty.", nameof(claims));
+
+			return await userManager.AddClaimsAsync(user, claims);
+		}
 
 		public async Task<IEnumerable<User>> GetAllUsersAsync()
         {
@@ -109,18 +159,54 @@ namespace LoadVantage.Core.Services
             }
         }
 
-        public async Task AssignUserRoleAsync(Guid userId, string role)
+        public async Task<ProfileViewModel> GetUserInformation(Guid userId)
         {
-            var user = await GetUserByIdAsync(userId);
-            var isAlreadyInRole = await userManager.IsInRoleAsync(user, role);
+	        var user = await GetUserByIdAsync(userId);
 
-            if (user != null && !isAlreadyInRole)
-            {
-                await userManager.AddToRoleAsync(user, role);
-            }
+	        if (user == null)
+	        {
+		        throw new Exception(UserNotFound);
+	        }
+
+			var viewModel = new ProfileViewModel
+	        {
+		        Id = user.Id.ToString(),
+		        Username = user.UserName!,
+		        Email = user.Email!,
+		        FirstName = user.FirstName,
+		        LastName = user.LastName,
+		        CompanyName = user.CompanyName!,
+		        Position = user.Position!,
+		        PhoneNumber = user.PhoneNumber!,
+		        UserImageUrl = user.UserImage!.ImageUrl,
+		        ChangePasswordViewModel = new ChangePasswordViewModel(),
+		        ImageFileUploadModel = new ImageFileUploadModel()
+	        };
+
+	        return viewModel;
+
         }
 
-        public async Task UpdateUserImageAsync(Guid userId, IFormFile file)
+		public async Task<UserChatViewModel> GetChatUserInfoAsync(Guid userId)
+        {
+	        var user = await context.Users
+		        .Include(u => u.UserImage)
+		        .Where(u => u.Id == userId)
+		        .Select(u => new UserChatViewModel
+		        {
+			        Id = u.Id,
+			        FullName = u.FullName,
+			        ProfilePictureUrl = u.UserImage.ImageUrl,
+			        PhoneNumber = u.PhoneNumber,
+			        Company = u.CompanyName
+		        })
+		        .AsNoTracking()
+		        .FirstOrDefaultAsync();
+
+	        return user;
+        }
+
+		public async Task UpdateUserImageAsync(Guid userId, IFormFile file)
         {
             var userImage = await context.UsersImages
                 .SingleOrDefaultAsync(ui => ui.UserId == userId);
