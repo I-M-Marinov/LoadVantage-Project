c67eed16062ded2b274066015df5946e1beb6e03
I-M-Marinov <lcfrrr@gmail.com>
2024-11-24 23:01:07 +0200

    Updated the ChatViewModal
    -- Added ProfileViewModel Profile property that would pass all the user information to the views that visualize user information

--- LoadVantage.Core/Models/Chat/ChatViewModel.cs
+++ LoadVantage.Core/Models/Chat/ChatViewModel.cs
@@ -11,10 +11,10 @@ namespace LoadVantage.Core.Models.Chat
 	public class ChatViewModel
 	{
 		public List<UserChatViewModel> Users { get; set; } 
-		public Guid? CurrentChatUserId { get; set; }
+		public Guid CurrentChatUserId { get; set; }
 		public List<ChatMessageViewModel> Messages { get; set; }
-		public UserChatViewModel BrokerInfo { get; set; }
-
+		public UserChatViewModel UserInfo { get; set; }
+		public ProfileViewModel Profile { get; set; }
 	}
 
 }
