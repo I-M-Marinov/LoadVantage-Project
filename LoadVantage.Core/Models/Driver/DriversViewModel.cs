37076f6e3d5b6733d2c9812dd04fa97949a183e3
I-M-Marinov <lcfrrr@gmail.com>
2024-11-24 23:21:53 +0200

    Added the DriversViewModel that would pass the profile information, all drivers and serve two empty models for adding a new driver or edit a driver to the ShowDrivers View

---
+++ LoadVantage.Core/Models/Driver/DriversViewModel.cs
@@ -0,0 +1,17 @@
+﻿using LoadVantage.Core.Models.Profile;
+using LoadVantage.Core.Models.Truck;
+
+
+namespace LoadVantage.Core.Models.Driver
+{
+	public class DriversViewModel
+	{
+		public ProfileViewModel Profile { get; set; } = null!;
+
+		public List<DriverViewModel> Drivers { get; set; } = null!;
+
+		public DriverViewModel NewDriver { get; set; } = null!; // For the Add Driver form
+		public DriverViewModel EditedDriver { get; set; } = null!; // For the Edit Driver form
+
+	}
+}
