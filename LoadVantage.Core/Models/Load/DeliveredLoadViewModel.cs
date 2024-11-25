5b2b8a12d8dccd1792b6b2ebc084de79dc29e29a
I-M-Marinov <lcfrrr@gmail.com>
2024-11-25 23:21:49 +0200

    Created the DeliveredLoadViewModel to map needed information and to visualize it in the LoadBoard Delivered Loads Tab ( mainly, but not only )

---
+++ LoadVantage.Core/Models/Load/DeliveredLoadViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace LoadVantage.Core.Models.Load
+{
+	public class DeliveredLoadViewModel
+	{
+		public Guid Id { get; set; } 
+
+		public string LoadLocations { get; set; } = null!;
+		public double? Distance { get; set; }
+		public decimal Price { get; set; }
+		public DateTime DeliveredOn { get; set; } 
+		public string BrokerName { get; set; } = null!;
+		public string DispatcherName { get; set; } = null!;
+		public string DriverName { get; set; } = null!;
+	}
+}
