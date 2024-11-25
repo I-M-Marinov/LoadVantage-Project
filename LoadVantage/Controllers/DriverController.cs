9051a2d8353220985b5cefe3ca3ea3e50da53b16
I-M-Marinov <lcfrrr@gmail.com>
2024-11-25 23:31:37 +0200

    Updated the Driver Controller
    -- Added GetAvailableDrivers, GetDriversWithTrucks and the AssignADriverToLoad method

--- LoadVantage/Controllers/DriverController.cs
+++ LoadVantage/Controllers/DriverController.cs
@@ -1,6 +1,7 @@
 ﻿using LoadVantage.Core.Contracts;
 using LoadVantage.Core.Models.Driver;
 using LoadVantage.Core.Models.Truck;
+using LoadVantage.Core.Services;
 using LoadVantage.Extensions;
 using LoadVantage.Filters;
 using Microsoft.AspNetCore.Authorization;
@@ -134,5 +135,66 @@ namespace LoadVantage.Controllers
 				return NotFound(DriverDoesNotExist);
 			}
 		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAvailableDrivers()
+		{
+			try
+			{
+				var availableDrivers = await driverService.GetAvailableDriversAsync();
+				var driverList = availableDrivers.Select(d => new
+				{
+					Id = d.DriverId,
+					Name = d.FullName
+				});
+
+				return Json(driverList); 
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, "An error occurred while retrieving drivers.");
+			}
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetDriversWithTrucks()
+		{
+			try
+			{
+				var drivers = await driverService.GetDriversWithTrucksAsync();
+				var driverList = drivers.Select(d => new
+				{
+					DriverId = d.DriverId, // Make sure the property names match the JavaScript expectations
+					Name = d.FullName
+				});
+
+				return Json(driverList);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, "An error occurred while retrieving drivers.");
+			}
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> AssignADriverToLoad(Guid loadId, Guid driverId)
+		{
+			Guid userId = User.GetUserId().Value; 
+
+			bool result = await driverService.AssignADriverToLoadAsync(loadId, driverId, userId);
+
+			if (!result)
+			{
+				TempData.SetErrorMessage(DriverWasNotAssignedToTheLoad);
+				return RedirectToAction("LoadDetails", "Load", new {loadId = loadId}); 
+			}
+
+			TempData.SetSuccessMessage(DriverWasAssignedToLoadSuccessfully);
+			return RedirectToAction("LoadDetails", "Load", new { loadId = loadId });
+		}
+
+
+
 	}
 }
