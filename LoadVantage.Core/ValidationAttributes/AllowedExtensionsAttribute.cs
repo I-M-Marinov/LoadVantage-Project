8a915be6a3a57efc0b498b3b023c08ac59f9af78
I-M-Marinov <lcfrrr@gmail.com>
2024-11-27 23:58:01 +0200

    Updated the AllowedExtensionsAttribute to differenciate between a user and administrator and apply different validation logic to the uploaded messages
    --> Administrators can upload a GIF image, Users cannot.

--- LoadVantage.Core/ValidationAttributes/AllowedExtensionsAttribute.cs
+++ LoadVantage.Core/ValidationAttributes/AllowedExtensionsAttribute.cs
@@ -7,21 +7,31 @@ namespace LoadVantage.Core.ValidationAttributes
 {
 	public class AllowedExtensionsAttribute : ValidationAttribute
     {
-        private static readonly string[] DefaultExtensions = ValidImageExtensions;
+		private readonly string[] _validExtensions;
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		public AllowedExtensionsAttribute()
 		{
-			var file = value as IFormFile;
-			if (file != null)
-			{
-				var extension = Path.GetExtension(file.FileName);
-				if (!DefaultExtensions.Contains(extension.ToLower()))
-				{
-					return new ValidationResult(ErrorMessage ?? "Invalid file extension.");
-				}
-			}
-
-			return ValidationResult.Success!;
+			_validExtensions = ValidImageExtensions;
 		}
+		public AllowedExtensionsAttribute(bool isAdmin)
+        {
+	        _validExtensions = isAdmin ? AdminValidImageExtensions : ValidImageExtensions;
+        }
+
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+	        var file = value as IFormFile;
+
+	        if (file != null)
+	        {
+		        var extension = Path.GetExtension(file.FileName).ToLower();
+		        if (!_validExtensions.Contains(extension))
+		        {
+			        return new ValidationResult(ErrorMessage ?? "Invalid file extension.");
+		        }
+	        }
+
+	        return ValidationResult.Success!;
+        }
 	}
 }
