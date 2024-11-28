67826a0529c4b4684f2a5fccd55d0d83af04796d
I-M-Marinov <lcfrrr@gmail.com>
2024-11-28 23:22:14 +0200

    Created the BrokerInfoVIewModel

---
+++ LoadVantage.Core/Models/Load/BrokerInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadVantage.Core.Models.Load
+{
+	public class BrokerInfoViewModel
+	{
+		public string? BrokerName { get; set; }
+		public string? BrokerEmail { get; set; }
+		public string? BrokerPhone { get; set; }
+	}
+}
+
