76416e6d699e8c00ca84753f3c384601af4824e0
I-M-Marinov <lcfrrr@gmail.com>
2024-11-28 23:07:24 +0200

    Created the AdminImageFileUploadModel
    Added the isAdmin true so that the custom attribute can pass the AdminValidImageExtensions as validation ( add the GIF to the list ), so that administrators can upload a GIF image as a UserImage

---
+++ LoadVantage/Areas/Admin/Models/Profile/AdminImageFileUploadModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LoadVantage.Core.ValidationAttributes;
+using static LoadVantage.Common.ValidationConstants.UserImageValidations;
+using static LoadVantage.Common.GeneralConstants.UserImage;
+
+namespace LoadVantage.Areas.Admin.Models.Profile
+{
+    public class AdminImageFileUploadModel
+    {
+        [Required(ErrorMessage = ImageMustBeSelected)]
+        [AllowedExtensions(isAdmin: true, ErrorMessage = AdminInvalidImageFileExtension)]
+        [FileSize(MaxSizeInBytes = UserImageMaxFileSize, ErrorMessage = ImageFileSizeExceeded)]
+        public IFormFile FormFile { get; set; } = null!;
+    }
+}
