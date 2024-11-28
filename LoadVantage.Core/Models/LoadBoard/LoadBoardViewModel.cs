95284140b3dbed92f6564bb1877be49376de4e60
I-M-Marinov <lcfrrr@gmail.com>
2024-11-28 22:53:36 +0200

    Removed the Firstname, Lastname, CompanyName and Position properties from the LoadBoardViewModel because they are passed already with the ProfileViewModel Profile

--- LoadVantage.Core/Models/LoadBoard/LoadBoardViewModel.cs
+++ LoadVantage.Core/Models/LoadBoard/LoadBoardViewModel.cs
@@ -6,10 +6,6 @@ namespace LoadVantage.Core.Models.LoadBoard
 	public class LoadBoardViewModel
 	{
 		public Guid UserId { get; set; }
-		public string FirstName { get; set; } = null!;
-		public string LastName { get; set; } = null!;
-		public string CompanyName { get; set; } = null!;
-		public string Position { get; set; } = null!;
 		public ProfileViewModel Profile { get; set; } = null!;
 		public IEnumerable<LoadViewModel> CreatedLoads { get; set; }
 		public IEnumerable<LoadViewModel> PostedLoads { get; set; }
