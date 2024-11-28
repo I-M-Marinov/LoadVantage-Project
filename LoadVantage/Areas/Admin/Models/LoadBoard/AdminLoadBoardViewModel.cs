1f16f8fd13db66da8d61f07a3d5f4436b275d4a2
I-M-Marinov <lcfrrr@gmail.com>
2024-11-28 23:16:02 +0200

    Created the AdminLoadBoardViewModel

---
+++ LoadVantage/Areas/Admin/Models/LoadBoard/AdminLoadBoardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LoadVantage.Areas.Admin.Models.Load;
+using LoadVantage.Areas.Admin.Models.Profile;
+using LoadVantage.Core.Models.Load;
+
+namespace LoadVantage.Areas.Admin.Models.LoadBoard
+{
+    public class AdminLoadBoardViewModel
+	{
+		public Guid UserId { get; set; }
+		public AdminProfileViewModel Profile { get; set; } = null!;
+		public IEnumerable<AdminLoadViewModel> CreatedLoads { get; set; }
+		public IEnumerable<AdminLoadViewModel> PostedLoads { get; set; }
+		public IEnumerable<AdminLoadViewModel> BookedLoads { get; set; }
+		public IEnumerable<AdminLoadViewModel> DeliveredLoads { get; set; }
+		public IEnumerable<AdminLoadViewModel> CancelledLoads { get; set; }
+		
+	}
+}
