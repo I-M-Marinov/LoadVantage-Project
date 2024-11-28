79564192f1edc6ed78fee0e6c5e6d2aa310cbd68
I-M-Marinov <lcfrrr@gmail.com>
2024-11-28 23:05:57 +0200

    Created the AdminChangePasswordViewModel

---
+++ LoadVantage/Areas/Admin/Models/Profile/AdminChangePasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static LoadVantage.Common.ValidationConstants.UserValidations;
+using static LoadVantage.Common.ValidationConstants.EditProfile;
+
+namespace LoadVantage.Areas.Admin.Models.Profile
+{
+    public class AdminChangePasswordViewModel
+    {
+
+        [Required(ErrorMessage = "Current password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string CurrentPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "New password is required.")]
+        [StringLength(NewPasswordMaxLength, MinimumLength = NewPasswordMinLength, ErrorMessage = NewPasswordLengthNotValid)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Confirmation of the new password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = NewAndConfirmPasswordDoNotMatch)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
