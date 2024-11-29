fcf64b61433ae50787935a0facc988c93956f379
I-M-Marinov <lcfrrr@gmail.com>
2024-11-29 21:56:11 +0200

    Created the UsersListModel that would serve all the information in the UserManagement View
    -- List of Users of type UserManagementViewModel
    -- NewUser of type AdminCreateUserViewModel to be used to create new users
    -- AdminProfileViewModel AdminProfile to serve all the information about the Administrar and his account
    -- SearchTerm for the search functionality in the View
    -- CurrentPage, PageSize and TotalUsers for the pagination functionality in the View

---
+++ LoadVantage/Areas/Admin/Models/User/UsersListModel.cs
@@ -0,0 +1,17 @@
+﻿using LoadVantage.Areas.Admin.Models.Profile;
+using LoadVantage.Areas.Admin.Services;
+
+namespace LoadVantage.Areas.Admin.Models.User
+{
+	public class UsersListModel
+	{
+		public IEnumerable<UserManagementViewModel> Users { get; set; } = new HashSet<UserManagementViewModel>();
+		public AdminCreateUserViewModel NewUser { get; set; } = null!;
+		public AdminProfileViewModel AdminProfile { get; set; } = null!;
+		public string SearchTerm { get; set; } = null!;
+		public int CurrentPage { get; set; }
+		public int PageSize { get; set; }
+		public int TotalUsers { get; set; } 
+
+	}
+}
