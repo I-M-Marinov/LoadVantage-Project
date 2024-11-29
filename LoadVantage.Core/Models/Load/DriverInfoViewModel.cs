0d458fac4c42a242f6d79207585c10a6d25af98d
I-M-Marinov <lcfrrr@gmail.com>
2024-11-29 22:01:06 +0200

    Removed the DriverTruckNumber property from the DriverInfoViewModel, as it is not needed. Logic around the completion of the loads revolves around the Drivers, not the Trucks

--- LoadVantage.Core/Models/Load/DriverInfoViewModel.cs
+++ LoadVantage.Core/Models/Load/DriverInfoViewModel.cs
@@ -3,7 +3,6 @@
     public class DriverInfoViewModel
     {
         public string? DriverName { get; set; }
-        public string? DriverTruckNumber { get; set; }
         public string? DriverLicenseNumber { get; set; }
     }
 }
