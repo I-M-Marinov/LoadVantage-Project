17955a1c6e207751e986e50d8cec2018640164ce
I-M-Marinov <lcfrrr@gmail.com>
2024-11-29 22:08:26 +0200

    Created the AdminCreateUserViewModel for the creation of new users through the Admin area's User Management

---
+++ LoadVantage/Areas/Admin/Models/User/AdminCreateUserViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static LoadVantage.Common.GeneralConstants.UserImage;
+using static LoadVantage.Common.ValidationConstants.UserValidations;
+using static LoadVantage.Common.ValidationConstants.EditProfile;
+
+namespace LoadVantage.Areas.Admin.Models.User
+{
+	public class AdminCreateUserViewModel
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public string? Role { get; set; }
+		[Required(ErrorMessage = UsernameRequired)]
+		[StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength, ErrorMessage = UserNameLengthNotValid)]
+		public required string UserName { get; set; }
+		[Required(ErrorMessage = EmailAddressRequired)]
+		[StringLength(EmailMaxLength, MinimumLength = EmailMinLength, ErrorMessage = EmailLengthNotValid)]
+		[EmailAddress(ErrorMessage = EmailAddressNotValid)]
+		[RegularExpression(EmailRegexPattern, ErrorMessage = EmailAddressNotValid)]
+		public required string Email { get; set; }
+		[Required(ErrorMessage = FirstNameRequired)]
+		[StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength, ErrorMessage = FirstNameLengthNotValid)]
+		public required string FirstName { get; set; }
+		[Required(ErrorMessage = LastNameRequired)]
+		[StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength, ErrorMessage = LastNameLengthNotValid)]
+		public required string LastName { get; set; }
+		[Required(ErrorMessage = CompanyNameRequired)]
+		[StringLength(CompanyNameMaxLength, MinimumLength = CompanyNameMinLength, ErrorMessage = CompanyNameLengthNotValid)]
+		public required string CompanyName { get; set; }
+		[Required]
+		public required string Position { get; set; }
+		[Required]
+		[StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength, ErrorMessage = PasswordLengthNotValid)]
+		[DataType(DataType.Password)]
+		public required string Password { get; set; }
+
+		[Required(ErrorMessage = PhoneNumberRequired)]
+		[StringLength(UserPhoneNumberMaxLength, MinimumLength = UserPhoneNumberMinLength, ErrorMessage = UserPhoneNumberLengthNotValid)]
+		[RegularExpression(PhoneNumberRegexPattern, ErrorMessage = UserPhoneNumberEmailAddressNotValid)]
+		public string PhoneNumber { get; set; } = string.Empty;
+		[Url]
+		[StringLength(UserImageMaxLength, ErrorMessage = ImageUrlExceedsLimit)]
+		public string? UserImageUrl { get; set; }
+		public string FullName => $"{FirstName} {LastName}";
+	}
+}
