2219516c47d7c43f77ecc4de009b6a47530016be
I-M-Marinov <lcfrrr@gmail.com>
2024-11-30 23:23:12 +0200

    Created the AdminEditUserViewModel to serve for Editing existing users in the User Management View

---
+++ LoadVantage/Areas/Admin/Models/User/AdminEditUserViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static LoadVantage.Common.ValidationConstants.UserValidations;
+using static LoadVantage.Common.ValidationConstants.EditProfile;
+
+namespace LoadVantage.Areas.Admin.Models.User
+{
+	public class AdminEditUserViewModel
+	{
+		public string Id { get; set; } 
+
+		[Required(ErrorMessage = UsernameRequired)]
+		[StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength, ErrorMessage = UserNameLengthNotValid)]
+		public required string UserName { get; set; }
+		[Required(ErrorMessage = EmailAddressRequired)]
+		[StringLength(EmailMaxLength, MinimumLength = EmailMinLength, ErrorMessage = EmailLengthNotValid)]
+		[EmailAddress(ErrorMessage = EmailAddressNotValid)]
+		[RegularExpression(EmailRegexPattern, ErrorMessage = EmailAddressNotValid)]
+		public required string Email { get; set; }
+		[Required(ErrorMessage = FirstNameRequired)]
+		[StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength, ErrorMessage = FirstNameLengthNotValid)]
+		public required string FirstName { get; set; }
+		[Required(ErrorMessage = LastNameRequired)]
+		[StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength, ErrorMessage = LastNameLengthNotValid)]
+		public required string LastName { get; set; }
+		[Required(ErrorMessage = CompanyNameRequired)]
+		[StringLength(CompanyNameMaxLength, MinimumLength = CompanyNameMinLength, ErrorMessage = CompanyNameLengthNotValid)]
+		public required string CompanyName { get; set; }
+
+		[Required(ErrorMessage = PhoneNumberRequired)]
+		[StringLength(UserPhoneNumberMaxLength, MinimumLength = UserPhoneNumberMinLength, ErrorMessage = UserPhoneNumberLengthNotValid)]
+		[RegularExpression(PhoneNumberRegexPattern, ErrorMessage = UserPhoneNumberEmailAddressNotValid)]
+		public string PhoneNumber { get; set; } = string.Empty;
+
+		public bool IsActive { get; set; }
+
+	}
+}
