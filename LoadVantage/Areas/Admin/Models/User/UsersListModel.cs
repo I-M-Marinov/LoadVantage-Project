a172640ee96337547893c2a565c6e3b95d985821
I-M-Marinov <lcfrrr@gmail.com>
2024-11-30 23:24:56 +0200

    Updated the UsersListModel
    --> added the AdminEditUserViewModel EditedUser model to this model to be used in the UserManagement view to edit existing users

--- LoadVantage/Areas/Admin/Models/User/UsersListModel.cs
+++ LoadVantage/Areas/Admin/Models/User/UsersListModel.cs
@@ -7,6 +7,7 @@ namespace LoadVantage.Areas.Admin.Models.User
 	{
 		public IEnumerable<UserManagementViewModel> Users { get; set; } = new HashSet<UserManagementViewModel>();
 		public AdminCreateUserViewModel NewUser { get; set; } = null!;
+		public AdminEditUserViewModel EditedUser { get; set; } = null!;
 		public AdminProfileViewModel AdminProfile { get; set; } = null!;
 		public string SearchTerm { get; set; } = null!;
 		public int CurrentPage { get; set; }
