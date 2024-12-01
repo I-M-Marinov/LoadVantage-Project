203f36e36769981856528ec4eb18c5bfc15456f2
I-M-Marinov <lcfrrr@gmail.com>
2024-12-01 23:38:22 +0200

    Created the AdminChatViewModel to work with the AdminChatController and AdminChatService

---
+++ LoadVantage/Areas/Admin/Models/AdminChat/AdminChatViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LoadVantage.Areas.Admin.Models.Profile;
+using LoadVantage.Core.Models.Chat;
+
+namespace LoadVantage.Areas.Admin.Models.Chat
+{
+	public class AdminChatViewModel
+	{
+		public List<UserChatViewModel> Users { get; set; }
+		public Guid CurrentChatUserId { get; set; }
+		public List<ChatMessageViewModel> Messages { get; set; }
+		public UserChatViewModel UserInfo { get; set; }
+		public AdminProfileViewModel Profile { get; set; }
+	}
+}
