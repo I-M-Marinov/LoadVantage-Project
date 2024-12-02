f4bd3c9463274061c8a28d4e68e64a4e6f6fee22
I-M-Marinov <lcfrrr@gmail.com>
2024-12-02 23:38:49 +0200

    Created the ErrorController to handle redirection when error occur
    -- Set up a test for the 404 page

---
+++ LoadVantage/Controllers/ErrorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoadVantage.Controllers
+{
+	public class ErrorController : Controller
+	{
+		// Default error handler
+		public IActionResult Error()
+		{
+			return View();
+		}
+
+		// Specific error handler for status codes
+		public IActionResult StatusCode(int statusCode)
+		{
+			switch (statusCode)
+			{
+				case 404:
+					return View("404"); // Views/Error/404.cshtml
+				case 500:
+					return View("500"); // Views/Error/500.cshtml
+				default:
+					return View("Error");
+			}
+		}
+
+		public IActionResult Test404()
+		{
+			return View("404"); // This will show the 404 page
+		}
+
+		public IActionResult Test500()
+		{
+			return View("500"); // This will show the 500 page
+		}
+	}
+
+}
