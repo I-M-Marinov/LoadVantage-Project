b41d07c6c6f21ff6f3c2f5023de26ba827eb7758
I-M-Marinov <lcfrrr@gmail.com>
2024-12-02 23:42:53 +0200

    Created the Statistics Controller

---
+++ LoadVantage/Areas/Admin/Controllers/StatisticsController.cs
@@ -0,0 +1,30 @@
+﻿using LoadVantage.Areas.Admin.Contracts;
+using LoadVantage.Areas.Admin.Models.Statistics;
+using LoadVantage.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadVantage.Areas.Admin.Controllers
+{
+	[Authorize]
+	[AdminOnly]
+	[Area("Admin")]
+	public class StatisticsController : Controller
+    {
+        private static IStatisticsService statisticsService;
+
+        public StatisticsController(IStatisticsService _statisticsService)
+        {
+			statisticsService = _statisticsService;
+        }
+
+		[HttpGet]
+		public async Task<IActionResult> Statistics(Guid adminId)
+        {
+
+            var model = await statisticsService.GetAllStatistics(adminId);
+
+			return View("~/Areas/Admin/Views/Admin/Statistics/Statistics.cshtml", model);
+		}
+	}
+}
