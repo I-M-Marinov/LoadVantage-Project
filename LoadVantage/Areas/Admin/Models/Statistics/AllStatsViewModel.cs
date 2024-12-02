43dea87459d5201235afdd697eb27d535f87f8f9
I-M-Marinov <lcfrrr@gmail.com>
2024-12-02 23:44:19 +0200

    Created the AllStatsViewModel

---
+++ LoadVantage/Areas/Admin/Models/Statistics/AllStatsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using LoadVantage.Areas.Admin.Models.Profile;
+
+namespace LoadVantage.Areas.Admin.Models.Statistics
+{
+	public class AllStatsViewModel
+    {
+        public AdminProfileViewModel Profile { get; set; } = null!;
+
+        public Dictionary<string, int> LoadCountsByStatus { get; set; } = new Dictionary<string, int>();
+
+        public int TotalUserCount { get; set; }
+        public int DispatcherCount { get; set; }
+        public int BrokerCount { get; set; }
+        public Dictionary<string, int> GroupedCompanyNames { get; set; } = new Dictionary<string, int>();
+
+        public int ActiveDrivers { get; set; }
+        public int FiredDrivers { get; set; }
+
+        public int AvailableTrucks { get; set; }
+        public int DecommissionedTrucks { get; set; }
+
+        public decimal TotalRevenues { get; set; }
+    }
+}
