d194633fec891cf7df97fae075cc187190dabd5e
I-M-Marinov <lcfrrr@gmail.com>
2024-12-03 23:58:17 +0200

    Updated the UserMangement Service and Interface
    -- Injected the htmlSanitizerService in the class to sanitize all string inputs before they are committed to the DB ( SearchUsersAsync, CreateAdministratorAsync, CreateUserAsync, UpdateUserAsync )
    
    -- Added logic in the DeactivateUserAsync
    ----> Dispatchers with active trucks cannot be deactivated
    ----> Brokers with active loads cannot be deactivated
    
    -- Added the ResetPasswordToDefaultAsync method that would reset the password for a user to the default set password

--- LoadVantage/Areas/Admin/Contracts/IUserManagementService.cs
+++ LoadVantage/Areas/Admin/Contracts/IUserManagementService.cs
@@ -1,20 +1,17 @@
 ﻿using LoadVantage.Areas.Admin.Models.User;
+using Microsoft.AspNetCore.Identity;
 
 namespace LoadVantage.Areas.Admin.Contracts
 {
 	public interface IUserManagementService
     {
-        Task<List<UserManagementViewModel>> GetUsersAsync(int pageNumber, int pageSize); 
-        Task<int> GetTotalUsersCountAsync();
-        Task<AdminCreateUserViewModel> CreateAdministratorAsync(AdminCreateUserViewModel model);
+        Task<List<UserManagementViewModel>> GetUsersAsync(int pageNumber, int pageSize);
+		Task<AdminCreateUserViewModel> CreateAdministratorAsync(AdminCreateUserViewModel model);
 		Task<AdminCreateUserViewModel> CreateUserAsync(AdminCreateUserViewModel userViewModel);       
 		Task<bool> DeactivateUserAsync(Guid userId);
 		Task<bool> ReactivateUserAsync(Guid userId);
 		Task<bool> UpdateUserAsync(AdminEditUserViewModel model);
 		Task<List<UserManagementViewModel>> SearchUsersAsync(string searchTerm);
-        Task<List<string>> GetUserRolesAsync();
-	
-
-
-	}
+		Task<IdentityResult> ResetPasswordToDefaultAsync(Guid userId);
+    }
 }
--- LoadVantage/Areas/Admin/Services/UserManagementService.cs
+++ LoadVantage/Areas/Admin/Services/UserManagementService.cs
@@ -1,21 +1,23 @@
-﻿using LoadVantage.Areas.Admin.Contracts;
-using LoadVantage.Areas.Admin.Models.User;
-using LoadVantage.Infrastructure.Data;
-using LoadVantage.Infrastructure.Data.Models;
+﻿using System.Security.Claims;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+using LoadVantage.Core.Contracts;
+using LoadVantage.Common.Enums;
+using LoadVantage.Infrastructure.Data.Contracts;
+using LoadVantage.Areas.Admin.Contracts;
+using LoadVantage.Areas.Admin.Models.User;
+using LoadVantage.Infrastructure.Data;
+using LoadVantage.Infrastructure.Data.Models;
+
+using Role = LoadVantage.Infrastructure.Data.Models.Role;
+
 using static LoadVantage.Common.GeneralConstants.UserRoles;
-using static LoadVantage.Common.GeneralConstants.SecretString;
+using static LoadVantage.Common.GeneralConstants.AdministratorManagement;
 using static LoadVantage.Common.GeneralConstants.ErrorMessages;
 using static LoadVantage.Common.GeneralConstants.UserImage;
-using CloudinaryDotNet.Actions;
-using Role = LoadVantage.Infrastructure.Data.Models.Role;
-using System.Data;
-using LoadVantage.Core.Contracts;
-using LoadVantage.Core.Services;
-using System.Security.Claims;
-using Microsoft.CodeAnalysis.CSharp.Formatting;
+
 
 namespace LoadVantage.Areas.Admin.Services
 {
@@ -25,13 +27,20 @@ namespace LoadVantage.Areas.Admin.Services
 		private readonly UserManager<BaseUser> userManager;
 		private readonly RoleManager<Role> roleManager;
 		private readonly IUserService userService;
+		private readonly IHtmlSanitizerService htmlSanitizer;
 
-		public UserManagementService(LoadVantageDbContext _context, UserManager<BaseUser> _userManager, RoleManager<Role> _roleManager, IUserService _userService)
+		public UserManagementService(
+			LoadVantageDbContext _context, 
+			UserManager<BaseUser> _userManager, 
+			RoleManager<Role> _roleManager, 
+			IUserService _userService,
+			IHtmlSanitizerService _htmlSanitizer)
 		{
 			context = _context;
 			userManager = _userManager;
 			roleManager = _roleManager;
 			userService = _userService;
+			htmlSanitizer = _htmlSanitizer;
 		}
 
         public async Task<List<UserManagementViewModel>> GetUsersAsync(int pageNumber , int pageSize)
@@ -68,7 +77,9 @@ namespace LoadVantage.Areas.Admin.Services
 
         public async Task<List<UserManagementViewModel>> SearchUsersAsync(string searchTerm)
         {
-            if (string.IsNullOrWhiteSpace(searchTerm))
+	        var sanitizedSearchTerm = htmlSanitizer.Sanitize(searchTerm);
+
+            if (string.IsNullOrWhiteSpace(sanitizedSearchTerm))
             {
 				var allUsers = await context.Users
                     .Include(u => u.UserImage)
@@ -92,7 +103,7 @@ namespace LoadVantage.Areas.Admin.Services
                 return allUsers;
             }
 
-            var normalizedSearchTerm = searchTerm.ToUpper(); 
+            var normalizedSearchTerm = sanitizedSearchTerm.ToUpper(); 
 
             var users = await context.Users
                 .Include(u => u.UserImage)
@@ -123,49 +134,46 @@ namespace LoadVantage.Areas.Admin.Services
             return users;
         }
 
-
-        public async Task<int> GetTotalUsersCountAsync()
-        {
-            return await context.Users.CountAsync();
-        }
-
         public async Task<AdminCreateUserViewModel> CreateAdministratorAsync(AdminCreateUserViewModel model)
 		{
+			var sanitizedFirstName = htmlSanitizer.Sanitize(model.FirstName);
+			var sanitizedLastName = htmlSanitizer.Sanitize(model.LastName);
+			var sanitizedUserName = htmlSanitizer.Sanitize(model.UserName);
+			var sanitizedCompanyName = htmlSanitizer.Sanitize(model.CompanyName);
+			var sanitizedPhoneNumber = htmlSanitizer.Sanitize(model.PhoneNumber);
+			var sanitizedEmail = htmlSanitizer.Sanitize(model.Email);
+			var sanitizedPassword = htmlSanitizer.Sanitize(model.Password);
+
 			if (model == null)
 			{
-				throw new ArgumentNullException("The model cannot be null");
+				throw new ArgumentNullException(ModelCannotBeNull);
 			}
 
 			var userRole = await roleManager.FindByNameAsync(AdminRoleName);
 
-			if (userRole == null)
-			{
-				throw new ArgumentNullException("Invalid role selected!");
-			}
-
 			var newUser = new Administrator()
 			{
 				Id = Guid.NewGuid(),
-				UserName = model.UserName,
-				Email = model.Email,
-				PhoneNumber = model.PhoneNumber,
+				UserName = sanitizedUserName,
+				Email = sanitizedEmail,
+				PhoneNumber = sanitizedPhoneNumber,
 				Position = AdminPositionName,
-				FirstName = model.FirstName,
-				LastName = model.LastName,
-				CompanyName = model.CompanyName,
+				FirstName = sanitizedFirstName,
+				LastName = sanitizedLastName,
+				CompanyName = sanitizedCompanyName,
 				UserImageId = DefaultImageId,
 				Role = userRole,
 				RoleId = userRole.Id
 			};
 
-			var result = await userManager.CreateAsync(newUser, model.Password);
-
+			var result = await userManager.CreateAsync(newUser, sanitizedPassword);
 
 			var roleResult = await userManager.AddToRoleAsync(newUser, AdminRoleName);
 			if (!roleResult.Succeeded)
 			{
 				var roleErrors = string.Join(Environment.NewLine, roleResult.Errors.Select(e => e.Description));
-				throw new InvalidOperationException($"Role assignment failed: {roleErrors}");
+
+				throw new InvalidOperationException(string.Format(RoleAssignmentFailed, roleErrors));
 			}
 
 			var claims = new List<Claim>
@@ -204,39 +212,42 @@ namespace LoadVantage.Areas.Admin.Services
 		{
 			if (model == null)
 			{
-				throw new ArgumentNullException("The model cannot be null");
+				throw new ArgumentNullException(ModelCannotBeNull);
 			}
+
+			var sanitizedFirstName = htmlSanitizer.Sanitize(model.FirstName);
+			var sanitizedLastName = htmlSanitizer.Sanitize(model.LastName);
+			var sanitizedUserName = htmlSanitizer.Sanitize(model.UserName);
+			var sanitizedCompanyName = htmlSanitizer.Sanitize(model.CompanyName);
+			var sanitizedPhoneNumber = htmlSanitizer.Sanitize(model.PhoneNumber);
+			var sanitizedEmail = htmlSanitizer.Sanitize(model.Email);
+			var sanitizedPassword = htmlSanitizer.Sanitize(model.Password);
 
 			var userRole = await roleManager.FindByNameAsync(UserRoleName);
-
-			if (userRole == null)
-			{
-				throw new ArgumentNullException("Invalid role selected!");
-			}
 
 			var newUser = new User
 			{
 				Id = Guid.NewGuid(),
-				UserName = model.UserName,
-				Email = model.Email,
-				PhoneNumber = model.PhoneNumber,
+				UserName = sanitizedUserName,
+				Email = sanitizedEmail,
+				PhoneNumber = sanitizedPhoneNumber,
 				Position = model.Position,
-				FirstName = model.FirstName,
-				LastName = model.LastName,
-				CompanyName = model.CompanyName,
+				FirstName = sanitizedFirstName,
+				LastName = sanitizedLastName,
+				CompanyName = sanitizedCompanyName,
 				UserImageId = DefaultImageId,
 				Role = userRole,
 				RoleId = userRole.Id
 			};
 
-			var result = await userManager.CreateAsync(newUser, model.Password);
+			var result = await userManager.CreateAsync(newUser, sanitizedPassword);
 
 
 			var roleResult = await userManager.AddToRoleAsync(newUser, UserRoleName);
 			if (!roleResult.Succeeded)
 			{
 				var roleErrors = string.Join(Environment.NewLine, roleResult.Errors.Select(e => e.Description));
-				throw new InvalidOperationException($"Role assignment failed: {roleErrors}");
+				throw new InvalidOperationException(string.Format(RoleAssignmentFailed, roleErrors));
 			}
 
 			var claims = new List<Claim>
@@ -283,12 +294,42 @@ namespace LoadVantage.Areas.Admin.Services
 				throw new ArgumentException(UserNotFound);
 			}
 
+			
+			if (user is Broker broker)
+			{
+				var hasLoadsInTransit = await context.Loads
+					.AnyAsync(l => l.BrokerId == broker.Id &&
+					               l.Status == LoadStatus.Booked &&
+					               l.BookedLoad.DriverId != null);
+
+				if (hasLoadsInTransit)
+				{
+					throw new InvalidOperationException(CannotDeactivateBroker);
+				}
+			}
+
+			if (user is Dispatcher dispatcher)
+			{
+				var hasActiveDrivers = await context.Drivers
+					.AnyAsync(d => d.DispatcherId == dispatcher.Id &&
+					               d.TruckId != null &&
+					               d.IsBusy); 
+
+				if (hasActiveDrivers)
+				{
+					throw new InvalidOperationException(CannotDeactivateDispatcher);
+				}
+			}
+
+			// Anonymize the user
+
 			user.FirstName = "N/A";
 			user.LastName = "N/A";
 			user.Email = "N/A";
 			user.CompanyName = "N/A";
 			user.NormalizedEmail = "N/A";
 			user.PhoneNumber = "N/A";
+
 			user.IsActive = false; // deactivate the account 
 
 			var userImageId = user.UserImage.Id;
@@ -326,19 +367,31 @@ namespace LoadVantage.Areas.Admin.Services
 
         public async Task<bool> UpdateUserAsync(AdminEditUserViewModel model)
         {
-	        var user = await userService.GetUserByIdAsync(Guid.Parse(model.Id));
+	        if (model == null)
+	        {
+		        throw new ArgumentNullException(ModelCannotBeNull);
+	        }
+
+			var user = await userService.GetUserByIdAsync(Guid.Parse(model.Id));
 
 	        if (user == null)
 	        {
 		        throw new InvalidOperationException(UserNotFound);
 	        }
 
-	        user.UserName = model.UserName;
-	        user.Email = model.Email;
-	        user.PhoneNumber = model.PhoneNumber;
-	        user.FirstName = model.FirstName;
-	        user.LastName = model.LastName;
-	        user.CompanyName = model.CompanyName;
+	        var sanitizedFirstName = htmlSanitizer.Sanitize(model.FirstName);
+	        var sanitizedLastName = htmlSanitizer.Sanitize(model.LastName);
+	        var sanitizedUserName = htmlSanitizer.Sanitize(model.UserName);
+	        var sanitizedCompanyName = htmlSanitizer.Sanitize(model.CompanyName);
+	        var sanitizedPhoneNumber = htmlSanitizer.Sanitize(model.PhoneNumber);
+	        var sanitizedEmail = htmlSanitizer.Sanitize(model.Email);
+
+			user.UserName = sanitizedUserName;
+	        user.Email = sanitizedEmail;
+	        user.PhoneNumber = sanitizedPhoneNumber;
+	        user.FirstName = sanitizedFirstName;
+	        user.LastName = sanitizedLastName;
+	        user.CompanyName = sanitizedCompanyName;
 
 
 	        var updateResult = await userService.UpdateUserAsync(user);
@@ -350,14 +403,27 @@ namespace LoadVantage.Areas.Admin.Services
 
 			return true;
 		}
-        
-		public async Task<List<string>> GetUserRolesAsync()
-		{
-			var roles = await context.Roles.Select(r => r.Name).ToListAsync();
-			return roles;
-		}
 
+        public async Task<IdentityResult> ResetPasswordToDefaultAsync(Guid userId)
+        {
+			var user = await userService.GetUserByIdAsync(userId);
 
+	        if (user == null)
+	        {
+		        throw new ArgumentException(UserNotFound);
+	        }
+
+	        var removePasswordResult = await userService.DeleteUserPassword(user);
+
+	        if (!removePasswordResult.Succeeded)
+	        {
+		        return removePasswordResult;
+	        }
+
+	        var addPasswordResult = await userService.AddUserDefaultPassword(user);
+
+	        return addPasswordResult;
+        }
 
 	}
 }
