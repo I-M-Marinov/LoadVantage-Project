80906da73b4854467ba225814127e9682a87a7c7
I-M-Marinov <lcfrrr@gmail.com>
2024-12-03 23:53:11 +0200

    Updated the AdminLoadBoard Service
    --> pulled all the queries for every load of a different Status and created private methods that return them as lists to improve readability and maintainability

--- LoadVantage/Areas/Admin/Services/AdminLoadBoardService.cs
+++ LoadVantage/Areas/Admin/Services/AdminLoadBoardService.cs
@@ -17,41 +17,96 @@ namespace LoadVantage.Areas.Admin.Services
 {
 	public class AdminLoadBoardService : IAdminLoadBoardService
 	{
-		private readonly LoadVantageDbContext context;
 		private readonly IAdminProfileService adminProfileService;
+		private readonly ILoadHelperService loadHelperService;
 
-		public AdminLoadBoardService(LoadVantageDbContext _context, IAdminProfileService _adminProfileService)
+		public AdminLoadBoardService(
+			IAdminProfileService _adminProfileService, 
+			ILoadHelperService _loadHelperService)
 		{
-			context = _context;
 			adminProfileService = _adminProfileService;
-		}
-
-		private async Task<IEnumerable<Load>> GetAllLoads(Guid userId)
-		{
-			var allLoads = await context.Loads
-				.Include(l => l.Broker)
-				.ThenInclude(b => b.UserImage)  
-				.Include(l => l.PostedLoad)
-				.Include(l => l.BookedLoad)
-				.ThenInclude(bl => bl.Driver) 
-				.Include(l => l.BookedLoad)
-				.ThenInclude(bl => bl.Dispatcher)  
-				.ThenInclude(d => d.UserImage)  
-				.Include(l => l.BookedLoad)
-				.ThenInclude(bl => bl.Dispatcher)
-				.ThenInclude(d => d.Trucks)   
-				.ThenInclude(t => t.Driver)    
-				.Include(l => l.DeliveredLoad)
-				.ToListAsync();
-
-
-			return allLoads;
+			loadHelperService = _loadHelperService;
 		}
 
 		public async Task<AdminLoadBoardViewModel> GetLoadBoardManager(Guid userId)
 		{
-			var allLoads = await GetAllLoads(userId);
+			var allLoads = await loadHelperService.GetAllLoads();
 
+			var createdLoads = GetCreatedLoads(allLoads);
+			var postedLoads = GetPostedLoads(allLoads);
+			var bookedLoads = GetBookedLoads(allLoads);
+			var deliveredLoads = GetDeliveredLoads(allLoads);
+			var cancelledLoads = GetCancelledLoads(allLoads);
+			var userProfile = await adminProfileService.GetAdminInformation(userId);
+
+			var loadBoardViewModel = new AdminLoadBoardViewModel()
+			{
+				UserId = userId,
+				CreatedLoads = createdLoads,
+				PostedLoads = postedLoads,
+				BookedLoads = bookedLoads,
+				DeliveredLoads = deliveredLoads,
+				CancelledLoads = cancelledLoads,
+				Profile = userProfile!
+			};
+
+			return loadBoardViewModel;
+		}
+
+		public async Task<IEnumerable<AdminLoadViewModel>> GetAllPostedLoadsAsync(Guid userId)
+		{
+			var postedLoads = await loadHelperService.GetAllLoads();
+			postedLoads = postedLoads.Where(l => l.Status == LoadStatus.Available);
+
+			var postedLoadsModels = postedLoads
+				.Select(load => new AdminLoadViewModel()
+				{
+					Id = load.Id,
+					OriginCity = load.OriginCity,
+					OriginState = load.OriginState,
+					DestinationCity = load.DestinationCity,
+					DestinationState = load.DestinationState,
+					PickupTime = load.PickupTime,
+					DeliveryTime = load.DeliveryTime,
+					PostedPrice = load.Price,
+					Distance = load.Distance,
+					Weight = load.Weight,
+					Status = load.Status.ToString(),
+					BrokerId = load.BrokerId,
+					BrokerInfo = load.BrokerId != Guid.Empty
+						? new BrokerInfoViewModel()
+						{
+							BrokerName = load.Broker.FullName,
+							BrokerEmail = load.Broker.Email,
+							BrokerPhone = load.Broker.PhoneNumber,
+						}
+						: null,
+					// Include Dispatcher Info if available
+					DispatcherInfo = load.BookedLoad?.DispatcherId != null
+						? new DispatcherInfoViewModel
+						{
+							DispatcherName = load.BookedLoad.Dispatcher.FullName,
+							DispatcherEmail = load.BookedLoad.Dispatcher.Email,
+							DispatcherPhone = load.BookedLoad.Dispatcher.PhoneNumber,
+						}
+						: null,
+
+					// Include Driver Info if available
+					DriverInfo = load.BookedLoad?.DriverId != null
+						? new DriverInfoViewModel
+						{
+							DriverName = load.BookedLoad.Driver?.FullName,
+							DriverLicenseNumber = load.BookedLoad?.Driver?.LicenseNumber
+						}
+						: null
+				})
+				.ToList();
+
+			return postedLoadsModels;
+		}
+
+		private List<AdminLoadViewModel> GetCreatedLoads(IEnumerable<Load> allLoads)
+		{
 			var createdLoads = allLoads
 				.Where(load => load.Status == LoadStatus.Created)
 				.OrderBy(l => l.PickupTime)
@@ -102,6 +157,11 @@ namespace LoadVantage.Areas.Admin.Services
 				})
 				.ToList();
 
+			return createdLoads;
+		}
+
+		private List<AdminLoadViewModel> GetPostedLoads(IEnumerable<Load> allLoads)
+		{
 			var postedLoads = allLoads
 				.Where(load => load.Status == LoadStatus.Available)
 				.OrderBy(l => l.PickupTime)
@@ -151,6 +211,11 @@ namespace LoadVantage.Areas.Admin.Services
 				})
 				.ToList();
 
+			return postedLoads;
+		}
+
+		private List<AdminLoadViewModel> GetBookedLoads(IEnumerable<Load> allLoads)
+		{
 			var bookedLoads = allLoads
 				.Where(load => load.Status == LoadStatus.Booked)
 				.OrderBy(l => l.PickupTime)
@@ -203,6 +268,11 @@ namespace LoadVantage.Areas.Admin.Services
 				})
 				.ToList();
 
+			return bookedLoads;
+		}
+
+		private List<AdminLoadViewModel> GetDeliveredLoads(IEnumerable<Load> allLoads)
+		{
 			var deliveredLoads = allLoads
 
 				.Where(load => load.Status == LoadStatus.Delivered)
@@ -211,7 +281,7 @@ namespace LoadVantage.Areas.Admin.Services
 				.ThenByDescending(l => l.OriginState)
 				.Select(load => new AdminLoadViewModel
 				{
-					
+
 					Id = load.Id,
 					OriginCity = load.OriginCity,
 					OriginState = load.OriginState,
@@ -258,8 +328,11 @@ namespace LoadVantage.Areas.Admin.Services
 				})
 				.ToList();
 
+			return deliveredLoads;
+		}
 
-
+		private List<AdminLoadViewModel> GetCancelledLoads(IEnumerable<Load> allLoads)
+		{
 			var cancelledLoads = allLoads
 				.Where(load => load.Status == LoadStatus.Cancelled)
 				.OrderBy(l => l.PickupTime)
@@ -287,94 +360,11 @@ namespace LoadVantage.Areas.Admin.Services
 							BrokerEmail = load.Broker.Email,
 							BrokerPhone = load.Broker.PhoneNumber
 						}
-						: null,
-					// Include Dispatcher Info if available
-					DispatcherInfo = load.BookedLoad?.DispatcherId != null
-						? new DispatcherInfoViewModel
-						{
-							DispatcherName = load.BookedLoad.Dispatcher.FullName,
-							DispatcherEmail = load.BookedLoad.Dispatcher.Email,
-							DispatcherPhone = load.BookedLoad.Dispatcher.PhoneNumber
-						}
-						: null,
-
-					// Include Driver Info if available
-					DriverInfo = load.BookedLoad?.DriverId != null
-						? new DriverInfoViewModel
-						{
-							DriverName = load.BookedLoad.Driver?.FullName,
-							DriverLicenseNumber = load.BookedLoad.Driver.LicenseNumber
-						}
 						: null
 				})
 				.ToList();
 
-			var userProfile = await adminProfileService.GetAdminInformation(userId);
-
-			var loadBoardViewModel = new AdminLoadBoardViewModel()
-			{
-				UserId = userId,
-				CreatedLoads = createdLoads,
-				PostedLoads = postedLoads,
-				BookedLoads = bookedLoads,
-				DeliveredLoads = deliveredLoads,
-				CancelledLoads = cancelledLoads,
-				Profile = userProfile
-			};
-
-			return loadBoardViewModel;
+			return cancelledLoads;
 		}
-
-		public async Task<IEnumerable<AdminLoadViewModel>> GetAllPostedLoadsAsync(Guid userId)
-		{
-			var postedLoads = await GetAllLoads(userId);
-			postedLoads = postedLoads.Where(l => l.Status == LoadStatus.Available);
-
-			var postedLoadsModels = postedLoads
-				.Select(load => new AdminLoadViewModel()
-				{
-					Id = load.Id,
-					OriginCity = load.OriginCity,
-					OriginState = load.OriginState,
-					DestinationCity = load.DestinationCity,
-					DestinationState = load.DestinationState,
-					PickupTime = load.PickupTime,
-					DeliveryTime = load.DeliveryTime,
-					PostedPrice = load.Price,
-					Distance = load.Distance,
-					Weight = load.Weight,
-					Status = load.Status.ToString(),
-					BrokerId = load.BrokerId,
-					BrokerInfo = load.BrokerId != Guid.Empty
-						? new BrokerInfoViewModel()
-						{
-							BrokerName = load.Broker.FullName,
-							BrokerEmail = load.Broker.Email,
-							BrokerPhone = load.Broker.PhoneNumber,
-						}
-						: null,
-					// Include Dispatcher Info if available
-					DispatcherInfo = load.BookedLoad?.DispatcherId != null
-						? new DispatcherInfoViewModel
-						{
-							DispatcherName = load.BookedLoad.Dispatcher.FullName,
-							DispatcherEmail = load.BookedLoad.Dispatcher.Email,
-							DispatcherPhone = load.BookedLoad.Dispatcher.PhoneNumber,
-						}
-						: null,
-
-					// Include Driver Info if available
-					DriverInfo = load.BookedLoad?.DriverId != null
-						? new DriverInfoViewModel
-						{
-							DriverName = load.BookedLoad.Driver?.FullName,
-							DriverLicenseNumber = load.BookedLoad.Driver.LicenseNumber
-						}
-						: null
-				})
-				.ToList();
-
-			return postedLoadsModels;
-		}
-	}
+ 	}
 }
