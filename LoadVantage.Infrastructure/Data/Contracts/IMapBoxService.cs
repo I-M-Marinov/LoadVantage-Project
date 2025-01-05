01e6c5f6ff27c143b83e976253647388cc022744
I-M-Marinov <lcfrrr@gmail.com>
2025-01-05 23:44:14 +0200

    Created the MapBoxService and interface

---
+++ LoadVantage.Infrastructure/Data/Contracts/IMapBoxService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadVantage.Infrastructure.Data.Contracts
+{
+	public interface IMapBoxService
+	{
+		string GetStaticMapUrl(double? originLatitude, double? originLongitude, double? destinationLatitude, double? destinationLongitude);
+	}
+}
---
+++ LoadVantage.Infrastructure/Data/Services/MapBoxService.cs
@@ -0,0 +1,50 @@
+﻿
+using LoadVantage.Infrastructure.Data.Contracts;
+using Microsoft.Extensions.Configuration;
+
+namespace LoadVantage.Infrastructure.Data.Services
+{
+	public class MapboxService: IMapBoxService
+	{
+		private readonly string _mapboxApiKey;
+
+		public MapboxService(IConfiguration configuration)
+		{
+			_mapboxApiKey = configuration["ApiKeys:Mapbox:AccessToken"];
+		}
+
+		public string GetStaticMapUrl(double? originLatitude, double? originLongitude, double? destinationLatitude, double? destinationLongitude)
+		{
+			var mapUrl = $"https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/";
+
+			mapUrl += $"pin-s-a+1ee80c({originLongitude},{originLatitude})"; 
+
+			if (destinationLatitude.HasValue && destinationLongitude.HasValue)
+			{
+				mapUrl += $",pin-s-b+eb0c26({destinationLongitude},{destinationLatitude})"; 
+			}
+
+			double minLongitude = Math.Min(originLongitude ?? 0, destinationLongitude ?? originLongitude ?? 0);
+			double minLatitude = Math.Min(originLatitude ?? 0, destinationLatitude ?? originLatitude ?? 0);
+			double maxLongitude = Math.Max(originLongitude ?? 0, destinationLongitude ?? originLongitude ?? 0);
+			double maxLatitude = Math.Max(originLatitude ?? 0, destinationLatitude ?? originLatitude ?? 0);
+
+			double centerLongitude = (minLongitude + maxLongitude) / 2;
+			double centerLatitude = (minLatitude + maxLatitude) / 2;
+			int zoomLevel = 5; 
+
+			mapUrl += $"/{centerLongitude},{centerLatitude},{zoomLevel}/1200x700";
+
+			mapUrl += $"?access_token={_mapboxApiKey}";
+
+			return mapUrl;
+		}
+
+
+
+
+
+
+	}
+
+}
